9e5f1d23da42ef1dc636e45338012554c3589c35
Programming Wiz <allisprogramming12789@gmail.com>
2026-03-18 02:46:11 +0700

    Initial commit

---
+++ 4 Queen's Puzzle/4 Queen's Puzzle/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class _4QueensPuzzle
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        cell00 = New Button()
+        cell01 = New Button()
+        cell02 = New Button()
+        cell03 = New Button()
+        cell07 = New Button()
+        cell06 = New Button()
+        cell05 = New Button()
+        cell04 = New Button()
+        cell15 = New Button()
+        cell14 = New Button()
+        cell13 = New Button()
+        cell12 = New Button()
+        cell11 = New Button()
+        cell10 = New Button()
+        cell09 = New Button()
+        cell08 = New Button()
+        startBtn = New Button()
+        restartBtn = New Button()
+        SuspendLayout()
+        ' 
+        ' cell00
+        ' 
+        cell00.Font = New Font("Microsoft Sans Serif", 36F)
+        cell00.Location = New Point(12, 12)
+        cell00.Name = "cell00"
+        cell00.Size = New Size(100, 100)
+        cell00.TabIndex = 0
+        cell00.UseVisualStyleBackColor = True
+        ' 
+        ' cell01
+        ' 
+        cell01.Font = New Font("Microsoft Sans Serif", 36F)
+        cell01.Location = New Point(118, 12)
+        cell01.Name = "cell01"
+        cell01.Size = New Size(100, 100)
+        cell01.TabIndex = 1
+        cell01.UseVisualStyleBackColor = True
+        ' 
+        ' cell02
+        ' 
+        cell02.Font = New Font("Microsoft Sans Serif", 36F)
+        cell02.Location = New Point(224, 12)
+        cell02.Name = "cell02"
+        cell02.Size = New Size(100, 100)
+        cell02.TabIndex = 2
+        cell02.UseVisualStyleBackColor = True
+        ' 
+        ' cell03
+        ' 
+        cell03.Font = New Font("Microsoft Sans Serif", 36F)
+        cell03.Location = New Point(330, 12)
+        cell03.Name = "cell03"
+        cell03.Size = New Size(100, 100)
+        cell03.TabIndex = 3
+        cell03.UseVisualStyleBackColor = True
+        ' 
+        ' cell07
+        ' 
+        cell07.Font = New Font("Microsoft Sans Serif", 36F)
+        cell07.Location = New Point(330, 118)
+        cell07.Name = "cell07"
+        cell07.Size = New Size(100, 100)
+        cell07.TabIndex = 7
+        cell07.UseVisualStyleBackColor = True
+        ' 
+        ' cell06
+        ' 
+        cell06.Font = New Font("Microsoft Sans Serif", 36F)
+        cell06.Location = New Point(224, 118)
+        cell06.Name = "cell06"
+        cell06.Size = New Size(100, 100)
+        cell06.TabIndex = 6
+        cell06.UseVisualStyleBackColor = True
+        ' 
+        ' cell05
+        ' 
+        cell05.Font = New Font("Microsoft Sans Serif", 36F)
+        cell05.Location = New Point(118, 118)
+        cell05.Name = "cell05"
+        cell05.Size = New Size(100, 100)
+        cell05.TabIndex = 5
+        cell05.UseVisualStyleBackColor = True
+        ' 
+        ' cell04
+        ' 
+        cell04.Font = New Font("Microsoft Sans Serif", 36F)
+        cell04.Location = New Point(12, 118)
+        cell04.Name = "cell04"
+        cell04.Size = New Size(100, 100)
+        cell04.TabIndex = 4
+        cell04.UseVisualStyleBackColor = True
+        ' 
+        ' cell15
+        ' 
+        cell15.Font = New Font("Microsoft Sans Serif", 36F)
+        cell15.Location = New Point(330, 330)
+        cell15.Name = "cell15"
+        cell15.Size = New Size(100, 100)
+        cell15.TabIndex = 15
+        cell15.UseVisualStyleBackColor = True
+        ' 
+        ' cell14
+        ' 
+        cell14.Font = New Font("Microsoft Sans Serif", 36F)
+        cell14.Location = New Point(224, 330)
+        cell14.Name = "cell14"
+        cell14.Size = New Size(100, 100)
+        cell14.TabIndex = 14
+        cell14.UseVisualStyleBackColor = True
+        ' 
+        ' cell13
+        ' 
+        cell13.Font = New Font("Microsoft Sans Serif", 36F)
+        cell13.Location = New Point(118, 330)
+        cell13.Name = "cell13"
+        cell13.Size = New Size(100, 100)
+        cell13.TabIndex = 13
+        cell13.UseVisualStyleBackColor = True
+        ' 
+        ' cell12
+        ' 
+        cell12.Font = New Font("Microsoft Sans Serif", 36F)
+        cell12.Location = New Point(12, 330)
+        cell12.Name = "cell12"
+        cell12.Size = New Size(100, 100)
+        cell12.TabIndex = 12
+        cell12.UseVisualStyleBackColor = True
+        ' 
+        ' cell11
+        ' 
+        cell11.Font = New Font("Microsoft Sans Serif", 36F)
+        cell11.Location = New Point(330, 224)
+        cell11.Name = "cell11"
+        cell11.Size = New Size(100, 100)
+        cell11.TabIndex = 11
+        cell11.UseVisualStyleBackColor = True
+        ' 
+        ' cell10
+        ' 
+        cell10.Font = New Font("Microsoft Sans Serif", 36F)
+        cell10.Location = New Point(224, 224)
+        cell10.Name = "cell10"
+        cell10.Size = New Size(100, 100)
+        cell10.TabIndex = 10
+        cell10.UseVisualStyleBackColor = True
+        ' 
+        ' cell09
+        ' 
+        cell09.Font = New Font("Microsoft Sans Serif", 36F)
+        cell09.Location = New Point(118, 224)
+        cell09.Name = "cell09"
+        cell09.Size = New Size(100, 100)
+        cell09.TabIndex = 9
+        cell09.UseVisualStyleBackColor = True
+        ' 
+        ' cell08
+        ' 
+        cell08.Font = New Font("Microsoft Sans Serif", 36F)
+        cell08.Location = New Point(12, 224)
+        cell08.Name = "cell08"
+        cell08.Size = New Size(100, 100)
+        cell08.TabIndex = 8
+        cell08.UseVisualStyleBackColor = True
+        ' 
+        ' startBtn
+        ' 
+        startBtn.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        startBtn.Location = New Point(451, 118)
+        startBtn.Name = "startBtn"
+        startBtn.Size = New Size(100, 100)
+        startBtn.TabIndex = 16
+        startBtn.Text = "Start"
+        startBtn.UseVisualStyleBackColor = True
+        ' 
+        ' restartBtn
+        ' 
+        restartBtn.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        restartBtn.Location = New Point(451, 224)
+        restartBtn.Name = "restartBtn"
+        restartBtn.Size = New Size(100, 100)
+        restartBtn.TabIndex = 16
+        restartBtn.Text = "Restart"
+        restartBtn.UseVisualStyleBackColor = True
+        ' 
+        ' _4QueensPuzzle
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(578, 450)
+        Controls.Add(restartBtn)
+        Controls.Add(startBtn)
+        Controls.Add(cell15)
+        Controls.Add(cell14)
+        Controls.Add(cell13)
+        Controls.Add(cell12)
+        Controls.Add(cell11)
+        Controls.Add(cell10)
+        Controls.Add(cell09)
+        Controls.Add(cell08)
+        Controls.Add(cell07)
+        Controls.Add(cell06)
+        Controls.Add(cell05)
+        Controls.Add(cell04)
+        Controls.Add(cell03)
+        Controls.Add(cell02)
+        Controls.Add(cell01)
+        Controls.Add(cell00)
+        Name = "_4QueensPuzzle"
+        Text = "4 Queen's Puzzle"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents cell00 As Button
+    Friend WithEvents cell01 As Button
+    Friend WithEvents cell02 As Button
+    Friend WithEvents cell03 As Button
+    Friend WithEvents cell07 As Button
+    Friend WithEvents cell06 As Button
+    Friend WithEvents cell05 As Button
+    Friend WithEvents cell04 As Button
+    Friend WithEvents cell15 As Button
+    Friend WithEvents cell14 As Button
+    Friend WithEvents cell13 As Button
+    Friend WithEvents cell12 As Button
+    Friend WithEvents cell11 As Button
+    Friend WithEvents cell10 As Button
+    Friend WithEvents cell09 As Button
+    Friend WithEvents cell08 As Button
+    Friend WithEvents startBtn As Button
+    Friend WithEvents restartBtn As Button
+
+End Class
---
+++ 4 Queen's Puzzle/4 Queen's Puzzle/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class _4QueensPuzzle
+    Dim n As Integer = 4, qCnt As Integer
+    Dim cells(n, n) As Button
+    Dim R(n), C(n), D1(2 * n - 1), D2(2 * n - 1) As Boolean
+
+    Private Sub _4QueensPuzzle_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim m As Integer = n ^ 2, d As Integer = 2 * n - 1
+        For i = 0 To m - 1
+            Dim r As Integer = i \ n, c As Integer = i Mod n
+            If i < 10 Then
+                cells(r, c) = DirectCast(Controls.Find("cell0" & i, False).First, Button)
+            Else
+                cells(r, c) = DirectCast(Controls.Find("cell" & i, False).First, Button)
+            End If
+            cells(r, c).Enabled = False
+            cells(r, c).Text = ""
+        Next
+        qCnt = 0
+        For i = 0 To d - 1
+            R(i \ 2) = True
+            C(i \ 2) = True
+            D1(i) = True
+            D2(i) = True
+        Next
+        startBtn.Enabled = True
+        restartBtn.Enabled = False
+    End Sub
+
+    Private Function IsAllOccupied(n As Integer, R() As Boolean, C() As Boolean, D1() As Boolean, D2() As Boolean)
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                If R(i) And C(j) And D1((n - 1) + i - j) And D2(i + j) Then
+                    Return False
+                End If
+            Next
+        Next
+        Return True
+    End Function
+
+    Private Sub GameOver()
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                cells(i, j).Enabled = False
+            Next
+        Next
+        restartBtn.Enabled = True
+    End Sub
+
+    Private Sub button_Click(sender As Object, e As EventArgs) Handles cell15.Click, cell14.Click, cell13.Click, cell12.Click, cell11.Click, cell10.Click, cell09.Click, cell08.Click, cell07.Click, cell06.Click, cell05.Click, cell04.Click, cell03.Click, cell02.Click, cell01.Click, cell00.Click
+        Dim cellName As String = CType(sender, Button).Name
+        Dim cellNum As Integer = CType(cellName.Substring(4), Integer)
+        Dim row As Integer = cellNum \ n, col = cellNum Mod n
+        If R(row) And C(col) And D1((n - 1) + row - col) And D2(row + col) Then
+            cells(row, col).Text = "Q"
+            cells(row, col).Enabled = False
+            qCnt += 1
+            R(row) = False
+            C(col) = False
+            D1((n - 1) + row - col) = False
+            D2(row + col) = False
+            If qCnt = n Then
+                GameOver()
+                MessageBox.Show("Solved!")
+            ElseIf IsAllOccupied(n, R, C, D1, D2) Then
+                GameOver()
+                MessageBox.Show("Game Over!")
+            End If
+        Else
+            MessageBox.Show("Occupied")
+        End If
+    End Sub
+
+    Private Sub startBtn_Click(sender As Object, e As EventArgs) Handles startBtn.Click
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                cells(i, j).Enabled = True
+            Next
+        Next
+        startBtn.Enabled = False
+    End Sub
+
+    Private Sub restartBtn_Click(sender As Object, e As EventArgs) Handles restartBtn.Click
+        Dim d As Integer = 2 * n - 1
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                cells(i, j).Text = ""
+            Next
+        Next
+        For i = 0 To d - 1
+            R(i \ 2) = True
+            C(i \ 2) = True
+            D1(i) = True
+            D2(i) = True
+        Next
+        qCnt = 0
+        startBtn.Enabled = True
+        restartBtn.Enabled = False
+    End Sub
+End Class
